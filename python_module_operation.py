53133d26c1a81f5ad612b042c9e5e4bc774ff685
Ravi <40563542+ravikumarwaghmare@users.noreply.github.com>
2020-05-16 13:28:14 +0530

    Update python_module_operation.py

--- python_module_operation.py
+++ python_module_operation.py
@@ -1,3 +1,9 @@
 help('modules')
 import math
 dir(math)
+
+'''10.1. Operating System Interface'''
+
+'''https://docs.python.org/3/tutorial/stdlib.html''
+
+#https://docs.python.org/3/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
